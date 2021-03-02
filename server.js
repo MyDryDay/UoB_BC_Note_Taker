ae2444a172c0d93fd0ca5a5e30d4935c6471acd2
George Cope <G.Cope13@gmail.com>
2021-03-02 13:58:11 +0000

    Declared routes for API, Registered the route, started the server & tested the route using Insomnia

--- server.js
+++ server.js
@@ -11,3 +11,19 @@ app.use(express.json());
 
 // Set the port
 const PORT = process.env.PORT || 8080;
+
+// Declare routes for API
+// Declare an instance of the express Router
+const router = express.Router();
+
+// Test the route
+router.get('/', (req, res) => {
+    res.json({ message: 'Success!' });
+});
+
+// Register routes for API
+app.use('/api', router);
+
+// Starting the server
+app.listen(PORT);
+console.log(`Listening on http://localhost:${PORT}`);
