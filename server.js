abacb5372bdbf9d4a7bbfc3a9fc9483c001fb387
George Cope <G.Cope13@gmail.com>
2021-03-02 14:19:21 +0000

    Added route for /public/notes.html page

--- server.js
+++ server.js
@@ -23,5 +23,9 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + '/public/index.html'))
 });
 
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname + '/public/notes.html'))
+});
+
 // Starting the server
 app.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`));
